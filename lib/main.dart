4cb7b6fe26e4011bd24bea0450d5b01ee4a09c7c
unknown <ayanasser1853@gmail.com>
2025-08-22 10:20:15 +0300

    background notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:pure_soft/core/routs/app_routs.dart';
+import 'package:pure_soft/core/services/notification_services.dart';
 import 'package:pure_soft/core/utils/app_color.dart';
+import 'package:pure_soft/firebase_options.dart';
 
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await NotificationServices.init();
   runApp(const MyApp());
 }
 
