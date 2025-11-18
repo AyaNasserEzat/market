83efd5dc67ffa2383daabf17eca08b9589924264
unknown <ayanasser1853@gmail.com>
2025-11-19 01:49:01 +0200

    add workflow

--- lib/feature/home/presentaion/views/widgets/banner_img.dart
+++ lib/feature/home/presentaion/views/widgets/banner_img.dart
@@ -3,28 +3,24 @@ import 'package:flutter/material.dart';
 
 class BannerImg extends StatelessWidget {
   const BannerImg({super.key, required this.img});
-final String img;
+  final String img;
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadiusGeometry.circular(13),
       child: CachedNetworkImage(
         imageUrl: 'https://fruits.sys-web.net/uploads/$img',
-  //width: double.infinity,
 
-        imageBuilder: (context, imageProvider) => Container(
-      decoration: BoxDecoration(
-      
-        image: DecorationImage(
-          image: imageProvider,
-          
-          fit: BoxFit.cover,
-        ),
-      ),
-        ),
-        placeholder: (context, url) => const Center(
-      child: CircularProgressIndicator(strokeWidth: 2),
-        ),
+        //width: double.infinity,
+        imageBuilder:
+            (context, imageProvider) => Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              ),
+            ),
+        placeholder:
+            (context, url) =>
+                const Center(child: CircularProgressIndicator(strokeWidth: 2)),
         errorWidget: (context, url, error) => const Icon(Icons.error),
       ),
     );
